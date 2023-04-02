ec9396b4001380711a997f8efeb3a90efd3a0e9c
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2023-04-02 15:24:07 +0000

    add home

---
+++ src/pages/home/home.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>My Website</h1>
+      </header>
+      <section>
+        <h2>About Us</h2>
+        <p>
+          We are a team of dedicated professionals with a passion for creating
+          innovative solutions. Our goal is to help businesses grow by providing
+          top-notch services and products tailored to their needs. We pride
+          ourselves on our commitment to quality and customer satisfaction.
+        </p>
+      </section>
+    </div>
+  );
+}
